2a1c514c8caea5f4ef6fe4a2ad799931aa26019a
BrtPx <brettkimeu@gmail.com>
2025-07-28 16:49:40 +0300

    ft: security

--- snpl/settings.py
+++ snpl/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-$+rt)$#lz12n!narehn&6l&ka9n3t(20a1i&nu=%37yxw+0erw"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["snpl.patazone.co.ke", "www.snpl.patazone.co.ke"]
 
