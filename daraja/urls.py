c431584b739f1cc0e58e397343293e1a127579a9
BrtPx <brettkimeu@gmail.com>
2025-07-29 11:08:31 +0300

    re: urls cleanup

--- daraja/urls.py
+++ daraja/urls.py
@@ -6,6 +6,4 @@ from daraja.views import STKCallback, STKexpress, Webhook, register_urls
 urlpatterns = [
     path("confirmation/", csrf_exempt(Webhook.as_view())),
     path("urls/", register_urls, name="register_urls"),
-    path("stkpush/", STKexpress, name="stkpush"),
-    path("callback/", csrf_exempt(STKCallback.as_view()), name="callback"),
 ]
